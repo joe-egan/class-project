55eb95fd9dfc64f21661b9da6719d0998052447a
joe-egan <144191133+joe-egan@users.noreply.github.com>
2023-12-10 17:46:58 -0600

    Update board.py

--- board.py
+++ board.py
@@ -1,16 +1,1 @@
-import turtle
-  
-t = turtle.Turtle()
-  
-# drawing first side
-t.forward(100) 
-t.left(90) 
-# drawing second side
-t.forward(100)
-t.left(90)
-# drawing third side
-t.forward(100) 
-t.left(90) 
-# drawing fourth side
-t.forward(100) 
-t.left(90) 
+
